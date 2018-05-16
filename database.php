edd3d59e42fddbbdb7f2fc2283d635d1670d7c9f
Lochitha <lochithaadikari@gmail.com>
2018-05-16 14:03:36 +0530

    changed database connectivity

--- database.php
+++ database.php
@@ -1,6 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="fmri_db";
 
-//connection making
-$mysqli= new mysqli('localhost','root','','fmri_db');
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
 
- ?>
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
